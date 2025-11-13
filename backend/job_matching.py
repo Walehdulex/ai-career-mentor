30d4ae36849cd5f6e1e229b807b4777c432ab9bb
Walehdulex <dulexw@gmail.com>
2025-11-13 19:55:37 +0000

    fixed import error

--- backend/job_matching.py
+++ backend/job_matching.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Tuple
 from sqlalchemy.orm import Session
-from database import User, JobPosting, JobMatch, UserJobPreferences, UserProfile
+from .database import User, JobPosting, JobMatch, UserJobPreferences, UserProfile
 import json
 
 class JobMatchingEngine:
